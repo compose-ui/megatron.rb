72a0700cf8d52daa56e632035dfe7cc01b616087
Brandon Mathis <brandon@imathis.com>
2016-10-14 11:56:29 -0500

    Fixed order for stylesheets, asset urls for favicon and pin_tab icon; added S3 upload task

--- app/helpers/megatron/application_helper.rb
+++ app/helpers/megatron/application_helper.rb
@@ -16,34 +16,6 @@ module Megatron
       end
     end
 
-    def embed_svg(filename, options = {})
-      group = 0
-      file = File.read(Engine.root.join('app', 'assets', 'images', 'megatron', filename))
-        .gsub(/<!--.+-->/, '')
-        .gsub(/^\t{2,}\s*<\/?g>/, '')
-        .gsub(/width=".+?"/, 'width="312"')
-        .gsub(/\sheight.+$/, '')
-        .gsub(/\t/, '  ')
-        .gsub(/\n{3,}/m, "\n")
-        .gsub(/^\s{2}<g>.+?^\s{2}<\/g>/m) { |g| 
-          g.gsub(/^\s{4,}\s+/, '    ')
-        }
-        .gsub(/^<g>.+?^<\/g>/m) { |g|
-          group += 1
-          count = 0
-          g.gsub(/^\s{2}<g>/) { |g| 
-            count += 1
-            %Q{  <g id="group-#{group}-cube-#{count}">}
-          }
-        }
-      # doc = Nokogiri::HTML::DocumentFragment.parse file
-      # svg = doc.at_css 'svg'
-      # if options[:class].present?
-      #   svg['class'] = options[:class]
-      # end
-      file.html_safe
-    end
-
     def add_class(string, *classes)
       string ||= ''
       string << " #{classes.join(' ')}"
