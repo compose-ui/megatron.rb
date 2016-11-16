8360c514977d2e58f8050d8fed9707fc437ab0d5
James Thorne <jthorne@compose.io>
2016-11-16 10:34:11 -0700

    chmod the credentials file

--- .buildkite/publish.sh
+++ .buildkite/publish.sh
@@ -3,6 +3,9 @@
 set -e
 
 echo "---\n:rubygems_api_key: $RUBYGEMS_API_KEY" > ~/.gem/credentials
+chmod 0600 ~/.gem/credentials
 
 echo '+++ Upload to rubygems'
 gem push megatron-`bundle exec ruby -e 'puts Gem.loaded_specs["megatron"].version'`.gem
+
+rm ~/.gem/credentials
