cee033ddf1c6bb12b7f479e89c97306b2782ac98
Brandon Mathis <brandon@imathis.com>
2017-01-30 17:06:20 -0600

    Another try at that upload task

--- .buildkite/publish.sh
+++ .buildkite/publish.sh
@@ -3,10 +3,10 @@
 set -e
 
 echo '+++ Publish to rubygems.org'
-curl --data-binary @megatron-`bundle exec ruby -e pkg/'puts Gem.loaded_specs["megatron"].version'`.gem \
+curl --data-binary @pkg/megatron-`bundle exec ruby -e 'puts Gem.loaded_specs["megatron"].version'`.gem \
   -H "Authorization:$RUBYGEMS_API_KEY" \
   -H "Content-Type: application/octet-stream" \
   https://rubygems.org/api/v1/gems
 
 echo '+++ Upload to CDN'
-bundle exec rake megatron:upload
+bundle exec rake upload
