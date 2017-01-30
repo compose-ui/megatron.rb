6df5ac24333bccd82969e6e399642013d87d87f6
Brandon Mathis <brandon@imathis.com>
2017-01-30 14:15:31 -0600

    Revert "Attempt to remove remnents of Rails 5"
    This reverts commit 9b40862e16489e57f5fa564f8241acbcaf36e5f2.

--- site/config/application.rb
+++ site/config/application.rb
@@ -1,10 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails'
+require "rails"
 # Pick the frameworks you want:
-require 'action_controller/railtie'
-require 'action_view/railtie'
-require 'autoprefixer-rails'
+require "action_controller/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+require "autoprefixer-rails"
+require "megatron/slim_code_filter"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
--- site/config/routes.rb
+++ site/config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get :status, to: proc { [200, {}, ['']] }
   resources :demo, param: :page, path: 'demo'
   resources :errors, param: :page, path: 'errors'
   resources :docs, param: :page, path: ''
